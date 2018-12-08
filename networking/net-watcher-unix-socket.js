c5f52b0fcb96003895037ae7ce9460408e22c945
Cristian Martinez <cristianandres92@gmail.com>
2018-12-08 16:26:38 -0500

    Listening on Unix socket

---
+++ networking/net-watcher-unix-socket.js
@@ -0,0 +1,23 @@
+'use strict'
+const fs = require('fs')
+const net = require('net')
+const filename = process.argv[2]
+
+if(!filename) {
+  throw Error('Error: No filename specified!')
+}
+
+net.createServer(connection => {
+  //Reporting
+  console.log('Subscriber connected.')
+  connection.write(`Now watching "${filename}" for changes...\n`)
+
+  //Watcher setup.
+  const watcher = fs.watch(filename, () => connection.write(`File changed: ${new Date()}\n`))
+
+  //Cleanup
+  connection.on('close', () => {
+    console.log('Subscriber disconnected.')
+    watcher.close()
+  })
+}).listen('/tmp/watcher.sock', () => console.log('Listening for subscribers...'))
