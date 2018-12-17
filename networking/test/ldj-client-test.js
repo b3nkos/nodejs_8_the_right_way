d483cfd46483a733878831f0cae2e2404fd004ec
Cristian Martinez <cristianandres92@gmail.com>
2018-12-17 00:49:59 -0500

    test for message event from split data events added, test for a message that is split over two data events added, test for validate error thrown when LDJClien contructor parameter is a null value

--- networking/test/ldj-client-test.js
+++ networking/test/ldj-client-test.js
@@ -21,4 +21,34 @@ describe('LDJClient', () => {
 
     stream.emit('data', '{"foo": "bar"}\n')
   })
+
+  it('should emit a message event from split data events', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, {foo: 'bar'})
+      done()
+    })
+
+    stream.emit('data', '{"foo":')
+    process.nextTick(() => stream.emit('data', '"bar"}\n'))
+  })
+
+  it('should emit a message event from a single message that is split over two data events', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, {foo: 'bar'})
+      done()
+    })
+
+    stream.emit('data', '{"foo":')
+    process.nextTick(() => stream.emit('data', '"bar"'))
+    process.nextTick(() => stream.emit('data', '}\n'))
+  })
+
+  it('should thrown a error when LDJClient constructor parameter is a null value', done => {
+    try {
+      LDJClient.connect(null)
+    } catch (error) {
+      assert.equal(error.message, "can't be a null value")
+      done() 
+    }
+  })
 })
