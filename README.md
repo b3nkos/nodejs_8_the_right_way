4d9d161f20107039ed4a6c77d568f87cb828e3ed
Cristian Martinez <cristianandres92@gmail.com>
2019-04-19 20:17:16 -0500

    chapter 3 name added to readme file

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ># Part one
 >>## Chapter 1 Getting up to speed on node.js 8
 >>## Chapter 2 wrangling the file System
->>> The code of this part is in folder `filesystem`
+>>> The code of this chapter is in folder `filesystem`
+>>## Chapter 3 Networking with Sockets
+>>> The code of this chapter is in folder `networking`
--- networking/net-watcher.js
+++ networking/net-watcher.js
@@ -1,23 +1,23 @@
-'use strict'
-const fs = require('fs')
-const net = require('net')
-const filename = process.argv[2]
+'use strict';
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
 
 if(!filename) {
-  throw Error('Error: No filename specified!')
+  throw Error('Error: No filename specified!');
 }
 
 net.createServer(connection => {
   //Reporting
-  console.log('Subscriber connected.')
-  connection.write(`Now watching "${filename}" for changes...\n`)
+  console.log('Subscriber connected.');
+  connection.write(`Now watching "${filename}" for changes...\n`);
 
   //Watcher setup.
-  const watcher = fs.watch(filename, () => connection.write(`File changed: ${new Date()}\n`))
+  const watcher = fs.watch(filename, () => connection.write(`File changed: ${new Date()}\n`));
 
   //Cleanup
   connection.on('close', () => {
-    console.log('Subscriber disconnected.')
-    watcher.close()
+    console.log('Subscriber disconnected.');
+    watcher.close();
   })
-}).listen(60300, () => console.log('Listening for subscribers...'))
+}).listen(60300, () => console.log('Listening for subscribers...'));
